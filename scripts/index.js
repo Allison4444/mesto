d4a59ba672de1deba3102efd4007a64135fb9853
Сергей Паньшин <low02345@bk.ru>
2022-12-18 16:14:15 +0500

    feat: написать логику для попапа

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,36 @@
-const popupOpenButtonElement = document.querySelector('.profile-info-row__edit-button');
 const popupElement = document.querySelector('.popup');
+const popupOpenButtonElement = document.querySelector('.profile-info-row__edit-button');
 const popupCloseButtonElement = popupElement.querySelector('.popup__container_element_close-button');
+const nameProfile = document.querySelector('.profile-info-row__name');
+const jobProfile = document.querySelector('.profile-info__job');
+const formElelement = document.querySelector('.popup__container');
+const nameInput = formElelement.querySelector('.popup__container_element_name');
+const jobInput = formElelement.querySelector('.popup__container_element_bio');
+
+nameInput.value = nameProfile.textContent;
+jobInput.value = jobProfile.textContent;
+
 const popupOpen = function () {
   popupElement.classList.add('popup_opened');
-}
+};
+
 const popupClose = function () {
   popupElement.classList.remove('popup_opened');
-}
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+};
+
 popupOpenButtonElement.addEventListener('click', popupOpen);
 popupCloseButtonElement.addEventListener('click', popupClose);
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  popupElement.classList.remove('popup_opened');
+}
+
+formElelement.addEventListener('submit', handleFormSubmit);
+
 
 
