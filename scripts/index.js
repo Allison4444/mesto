e34590d890f8ae4e47ad0c761e910b5f92538d50
Сергей Паньшин <low02345@bk.ru>
2022-12-19 18:31:09 +0500

    fix: удалить высоту у текстовых блоков

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const popupElement = document.querySelector('.popup');
 const popupOpenButtonElement = document.querySelector('.profile-info-row__edit-button');
-const popupCloseButtonElement = popupElement.querySelector('.popup__container_element_close-button');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
 const nameProfile = document.querySelector('.profile-info-row__name');
 const jobProfile = document.querySelector('.profile-info__job');
 const formElelement = document.querySelector('.popup__container');
-const nameInput = formElelement.querySelector('.popup__container_element_name');
-const jobInput = formElelement.querySelector('.popup__container_element_bio');
+const nameInput = formElelement.querySelector('.popup__input_element_name');
+const jobInput = formElelement.querySelector('.popup__input_element_job');
 
 nameInput.value = nameProfile.textContent;
 jobInput.value = jobProfile.textContent;
