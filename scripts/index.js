90fdf753f0941c960e3b63414e51ddeb32b3071d
Сергей Паньшин <low02345@bk.ru>
2022-12-20 17:26:08 +0500

    fix: исправить ошибки после первой итерации

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,59 @@
+// Находим попап в DOM
 const popupElement = document.querySelector('.popup');
-const popupOpenButtonElement = document.querySelector('.profile-info-row__edit-button');
+
+// Находим кнопку для открытия попапа в DOM
+const popupOpenButtonElement = document.querySelector('.profile__info-row-edit-button');
+
+// Находим кнопку для закрытия попапа в DOM
 const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
-const nameProfile = document.querySelector('.profile-info-row__name');
-const jobProfile = document.querySelector('.profile-info__job');
-const formElelement = document.querySelector('.popup__container');
-const nameInput = formElelement.querySelector('.popup__input_element_name');
-const jobInput = formElelement.querySelector('.popup__input_element_job');
 
-nameInput.value = nameProfile.textContent;
-jobInput.value = jobProfile.textContent;
+// Находим имя в профаиле в DOM
+let nameProfile = document.querySelector('.profile__info-row-name');
 
-const popupOpen = function () {
+// Находим работу в профаиле в DOM
+let jobProfile = document.querySelector('.profile__info-job');
+
+// Находим форму в DOM
+let formElelement = document.querySelector('.popup__form');
+
+// Находим поля формы в DOM
+let nameInput = formElelement.querySelector('.popup__input_element_name');
+let jobInput = formElelement.querySelector('.popup__input_element_job');
+
+// Функция для открытия попапа
+let openPopup = function () {
+  // Добавляем попапу класс для его открытия
   popupElement.classList.add('popup_opened');
-};
 
-const popupClose = function () {
-  popupElement.classList.remove('popup_opened');
+  // При открытии заполняем поля уже имеющейся информацией на странице
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 };
 
-popupOpenButtonElement.addEventListener('click', popupOpen);
-popupCloseButtonElement.addEventListener('click', popupClose);
+// Функция для закрытия попапа
+let closePopup = function () {
+  // Удаляем попапу класс для его закрытия
+  popupElement.classList.remove('popup_opened');
+};
 
+// Обработчик отправки формы (пока что ее никуда не отправляет!)
 function handleFormSubmit(evt) {
+  // Отмена стандартной отправки формы
   evt.preventDefault();
+
+  // Заменяем информацию в профаиле на ту, которую указали в форме
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popupElement.classList.remove('popup_opened');
+
+  // Закрываем попап
+  closePopup()
 }
 
+// Прикрепляем слушатель на кнопку открытия попапа
+popupOpenButtonElement.addEventListener('click', openPopup);
+
+// Прикрепляем слушатель на кнопку закрытия попапа
+popupCloseButtonElement.addEventListener('click', closePopup);
+
+// Прикрепляем обработчик к форме, который следит за отправкой
 formElelement.addEventListener('submit', handleFormSubmit);
-
-
-
